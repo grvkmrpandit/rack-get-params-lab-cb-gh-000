87535c14bd736889889c86a2322b17674e86b4a6
Gaurav Pandit <grvkmrpandit@gmail.com>
2019-08-12 04:28:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,26 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart=["chuby","busty","bigtits"]
+  
+  
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    nreq = Rack::Request.new(env)
+    if nreq.path.match(/cart/)
+      @@cart.each do |cart|
+        resp.write"#{cart}\n"
+      end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    else
+      resp.write "Path Not Found"
+    end
+
+      
 
     if req.path.match(/items/)
       @@items.each do |item|
@@ -21,7 +37,9 @@ class Application
   end
 
   def handle_search(search_term)
-    if @@items.include?(search_term)
+    if @@cart.include?(search_term)
+      return "#{search_term} mil gaya laura"
+    elsif @@items.include?(search_term)
       return "#{search_term} is one of our items"
     else
       return "Couldn't find #{search_term}"
